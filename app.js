e96a1fc3845ed65f5a8e35c0277cc960c3d74c60
GyoGaspar <gyovanna.gaspar4@gmail.com>
2025-03-21 21:49:40 -0300

    update

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ titulo.innerHTML = 'Jogo de Adivinhação';
 let subtitulo = document.querySelector('p');
 subtitulo.innerHTML = ' Escolha um número de 1 e 100';
 
+let tentativas = 3;
+
 exibirTextoNaTela('h1', 'Jogo de adivinhação');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
 function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * 100) + 1;
+    return parseInt(Math.random() * 10) + 1;
 } 
 
 let numeroSecreto = gerarNumeroAleatorio();
@@ -21,6 +23,19 @@ function exibirTextoNaTela(tag, texto) {
 
 function verificarChute() {
     let chute = parseInt (document.querySelector('input').value);
-    console.log(chute  === numeroSecreto);
+    if(tentativas === 0){
+        exibirTextoNaTela('h1', 'Suas tentativas acabaram!');
+    } else {
+        tentativas--;
+        let palavraTentativa = tentativas === 1? 'tentativa' : 'tentativas';
+    if(chute  === numeroSecreto) {
+      exibirTextoNaTela ('h1', 'Parabéns! Você acertou!');
+    } else if (chute > numeroSecreto){
+        exibirTextoNaTela('h1', `Errou! o numero secreto é menor.
+            Você ainda tem ${tentativas} ${palavraTentativa}`);
+    } else {
+        exibirTextoNaTela('h1', `Errou! o numero secreto é maior.
+        Você ainda tem ${tentativas} ${palavraTentativa}.`);
+    }
 }
-
+}
